9ac5d720f0aa2bbbd08f0493b9977e75bdd803fe
George Witteman <george@witteman.me>
2023-04-25 14:31:25 +0000

    Add TypeScript

--- src/add.test.ts
+++ src/add.test.ts
@@ -1,9 +1,15 @@
 import { describe, it } from "node:test";
 import { strict as assert } from "node:assert";
-import { add } from "./add.js";
+import { add, add3 } from "./add.js";
 
 describe("add", () => {
   it("should add two numbers", () => {
     assert.equal(add(1, 1), 2);
+  });
+});
+
+describe("add3", () => {
+  it("should add three numbers", () => {
+    assert.equal(add3(1, 2, 3), 6);
   });
 });
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as dns from "node:dns";
+import * as http from "node:http";
+import * as util from "node:util";
+
+async function handler(req: http.IncomingMessage, res: http.ServerResponse) {
+  const url = new URL(req.url!, `http://${req.headers.host}`);
+  console.log("URL: %s", url.toString());
+  res.writeHead(10 / 0, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      status: res.statusCode,
+      statusCode: http.STATUS_CODES[res.statusCode],
+      data: "Hello World!",
+      "google.com": await util.promisify(dns.lookup)("google.com"),
+    })
+  );
+}
+
+const server = http.createServer(handler);
+
+server.listen(8000, () => {
+  const address = server.address();
+  const port =
+    typeof address === "object" && address && "port" in address
+      ? address.port
+      : address;
+  console.log(`Listening on port ${port}`);
+});
+
+console.log(
+  Object.fromEntries(server.eventNames().map((e) => [e, server.listeners(e)]))
+);
+
+Promise.any([Promise.resolve(true), Promise.reject()]);
