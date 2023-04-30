14bbddfa96f9863ae941646269b4815976bb15a3
George Witteman <george@witteman.me>
2023-04-30 04:26:13 +0000

    progress

--- src/app.ts
+++ src/app.ts
@@ -24,65 +24,83 @@ export class Response {
   }
 }
 
-export class Request<M extends Method, Q> {
+export class Request<M extends Method, Q, J> {
   query: Q;
   url: URL;
+  pathParts: string[];
   method: M;
+  json: J;
 
-  constructor(url: URL, method: M, query: Q) {
+  constructor(url: URL, method: M, pathParts: string[], query: Q, json: J) {
     this.url = url;
     this.method = method;
+    this.pathParts = pathParts;
     this.query = query;
+    this.json = json;
   }
 }
 
-type RouteOptions<M extends Method, Q> = {
-  path: string;
+type RouteOptions<M extends Method, Q, J> = {
+  path: z.ZodTuple;
   method: M;
   query?: z.ZodType<Q>;
+  json?: z.ZodType<J>;
 };
 
-export class Route<M extends Method, Q> {
-  options: RouteOptions<M, Q>;
-  handler: (req: Request<M, Q>, res: Response) => Promise<void>;
+export class Route<M extends Method, Q, J> {
+  options: RouteOptions<M, Q, J>;
+  handler: (req: Request<M, Q, J>, res: Response) => Promise<void>;
 
   constructor(
-    options: RouteOptions<M, Q>,
-    handler: (req: Request<M, Q>, res: Response) => Promise<void>
+    options: RouteOptions<M, Q, J>,
+    handler: (req: Request<M, Q, J>, res: Response) => Promise<void>
   ) {
     this.options = options;
     this.handler = handler;
   }
 
-  match(path: string, method: string, query: unknown) {
+  match(path: string[], method: string, query: unknown, json: unknown) {
     return (
       this.options.method === method &&
-      this.options.path === path &&
-      (this.options.query?.safeParse(query).success ?? true)
+      this.options.path.safeParse(path).success &&
+      (this.options.query?.safeParse(query).success ?? true) &&
+      (this.options.json?.safeParse(json).success ?? true)
     );
   }
+}
+
+async function rawBody(req: http.IncomingMessage): Promise<string> {
+  const chunks: Buffer[] = [];
+  return new Promise<string>((resolve, reject) => {
+    req.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+    req.on("error", (err) => reject(err));
+    req.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+  });
 }
 
 // https://github.com/farrow-js/farrow/blob/master/examples/example/src/api/todos.ts
 export class App {
   server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
-  routes: Route<Method, unknown>[];
+  routes: Route<Method, unknown, unknown>[];
 
   constructor() {
     this.server = http.createServer(this.handleRequest.bind(this));
     this.routes = [];
   }
 
-  route<M extends Method, Q>(
-    options: RouteOptions<M, Q>,
-    handler: (req: Request<M, Q>, res: Response) => Promise<void>
+  route<M extends Method, Q, J>(
+    options: RouteOptions<M, Q, J>,
+    handler: (req: Request<M, Q, J>, res: Response) => Promise<void>
   ) {
     const route = new Route(options, handler);
     // TODO: Can we get rid of this any?
     this.routes.push(route as any);
   }
 
-  handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+  async handleRequestInner(
+    req: http.IncomingMessage,
+    res: http.ServerResponse
+  ) {
     const method = req.method;
     const host = req.headers.host;
     const rawUrl = req.url;
@@ -93,8 +111,12 @@ export class App {
     }
     const url = new URL(rawUrl, `http://${host}`);
     const query = Object.fromEntries(url.searchParams.entries());
+    const pathParts = url.pathname.slice(1).split("/");
+    const body = await rawBody(req);
+    const json = JSON.parse(body);
+    console.log(pathParts, method, query, json);
     const route = this.routes.find((route) =>
-      route.match(url.pathname, method, query)
+      route.match(pathParts, method, query, json)
     );
     if (!route) {
       res.writeHead(404);
@@ -102,16 +124,18 @@ export class App {
       return;
     }
     const parsedQuery = route.options.query?.parse(query);
-    route
-      .handler(
-        new Request(url, route.options.method, parsedQuery),
-        new Response(res)
-      )
-      .catch((e) => {
-        console.error(e);
-        res.writeHead(500);
-        res.end(e.message);
-      });
+    await route.handler(
+      new Request(url, route.options.method, pathParts, parsedQuery, json),
+      new Response(res)
+    );
+  }
+
+  handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
+    this.handleRequestInner(req, res).catch((e) => {
+      console.error(e);
+      res.writeHead(500);
+      res.end(e.message);
+    });
   }
 
   listen() {
--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,40 @@ import { App } from "./app.js";
 const app = new App();
 
 app.route(
-  { path: "/", method: "GET", query: z.object({ foo: z.string() }) },
+  {
+    path: z.tuple([z.literal("")]),
+    method: "GET",
+    query: z.object({ foo: z.string() }),
+  },
   async (req, res) => {
     res.send(200, req.query.foo);
   }
 );
 
-app.route({ path: "/foo", method: "GET" }, async (req, res) => {
-  res.send(200, `${req.method}: ${req.url.href}`);
-});
+app.route(
+  { path: z.tuple([z.literal("foo")]), method: "GET" },
+  async (req, res) => {
+    res.send(200, `${req.method}: ${req.url.href}`);
+  }
+);
 
-app.route({ path: "/fail", method: "GET" }, async (req, res) => {
-  // @ts-expect-error
-  asdf.asdf;
-  res.send(200, `${req.method}: ${req.url.href}`);
-});
+app.route(
+  {
+    path: z.tuple([z.literal("foo"), z.string(), z.literal("bar")]),
+    method: "GET",
+  },
+  async (req, res) => {
+    res.send(200, `${req.method}: ${req.url.href} - ${req.pathParts}`);
+  }
+);
+
+app.route(
+  { path: z.tuple([z.literal("fail")]), method: "GET" },
+  async (req, res) => {
+    // @ts-expect-error
+    asdf.asdf;
+    res.send(200, `${req.method}: ${req.url.href}`);
+  }
+);
 
 app.listen();
