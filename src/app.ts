abddc7de1033ec86535f89d95c79eab66a74ef52
George Witteman <george@witteman.me>
2023-04-30 03:56:45 +0000

    progress

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import * as http from "node:http";
 import { z } from "zod";
 
+type Method = "GET" | "POST" | "OPTIONS";
+
 export class Response {
   private response: http.ServerResponse;
 
@@ -22,62 +24,94 @@ export class Response {
   }
 }
 
-export class Request<Q> {
+export class Request<M extends Method, Q> {
   query: Q;
+  url: URL;
+  method: M;
 
-  constructor(query: Q) {
+  constructor(url: URL, method: M, query: Q) {
+    this.url = url;
+    this.method = method;
     this.query = query;
   }
 }
 
-type RouteOptions<Q> = {
+type RouteOptions<M extends Method, Q> = {
   path: string;
-  method: "GET" | "POST";
+  method: M;
   query?: z.ZodType<Q>;
 };
 
-export class Route<Q> {
-  options: RouteOptions<Q>;
-  handler: (req: Request<Q>, res: Response) => Promise<void>;
+export class Route<M extends Method, Q> {
+  options: RouteOptions<M, Q>;
+  handler: (req: Request<M, Q>, res: Response) => Promise<void>;
 
   constructor(
-    options: RouteOptions<Q>,
-    handler: (req: Request<Q>, res: Response) => Promise<void>
+    options: RouteOptions<M, Q>,
+    handler: (req: Request<M, Q>, res: Response) => Promise<void>
   ) {
     this.options = options;
     this.handler = handler;
   }
+
+  match(path: string, method: string, query: unknown) {
+    return (
+      this.options.method === method &&
+      this.options.path === path &&
+      (this.options.query?.safeParse(query).success ?? true)
+    );
+  }
 }
 
+// https://github.com/farrow-js/farrow/blob/master/examples/example/src/api/todos.ts
 export class App {
   server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
-  routes: Route<unknown>[];
+  routes: Route<Method, unknown>[];
 
   constructor() {
     this.server = http.createServer(this.handleRequest.bind(this));
     this.routes = [];
   }
 
-  route<Q>(route: Route<Q>) {
+  route<M extends Method, Q>(
+    options: RouteOptions<M, Q>,
+    handler: (req: Request<M, Q>, res: Response) => Promise<void>
+  ) {
+    const route = new Route(options, handler);
     // TODO: Can we get rid of this any?
     this.routes.push(route as any);
   }
 
   handleRequest(req: http.IncomingMessage, res: http.ServerResponse) {
-    const url = new URL(req.url!, `http://${req.headers.host}`);
+    const method = req.method;
+    const host = req.headers.host;
+    const rawUrl = req.url;
+    if (!rawUrl || !host || !method) {
+      res.writeHead(400);
+      res.end("missing url, host, or method");
+      return;
+    }
+    const url = new URL(rawUrl, `http://${host}`);
     const query = Object.fromEntries(url.searchParams.entries());
-    const route = this.routes.find(
-      (route) =>
-        route.options.method === req.method &&
-        route.options.path === url.pathname &&
-        (route.options.query?.safeParse(query).success ?? true)
+    const route = this.routes.find((route) =>
+      route.match(url.pathname, method, query)
     );
     if (!route) {
       res.writeHead(404);
       res.end("no route found");
       return;
     }
-    route.handler(new Request(query), new Response(res));
+    const parsedQuery = route.options.query?.parse(query);
+    route
+      .handler(
+        new Request(url, route.options.method, parsedQuery),
+        new Response(res)
+      )
+      .catch((e) => {
+        console.error(e);
+        res.writeHead(500);
+        res.end(e.message);
+      });
   }
 
   listen() {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { z } from "zod";
-import { App, Route } from "./app.js";
+import { App } from "./app.js";
 
 const app = new App();
 
 app.route(
-  new Route(
-    { path: "/", method: "GET", query: z.object({ foo: z.string() }) },
-    async (req, res) => {
-      res.send(200, req.query.foo);
-    }
-  )
+  { path: "/", method: "GET", query: z.object({ foo: z.string() }) },
+  async (req, res) => {
+    res.send(200, req.query.foo);
+  }
 );
+
+app.route({ path: "/foo", method: "GET" }, async (req, res) => {
+  res.send(200, `${req.method}: ${req.url.href}`);
+});
+
+app.route({ path: "/fail", method: "GET" }, async (req, res) => {
+  // @ts-expect-error
+  asdf.asdf;
+  res.send(200, `${req.method}: ${req.url.href}`);
+});
 
 app.listen();
